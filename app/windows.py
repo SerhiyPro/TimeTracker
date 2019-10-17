0f925f4aff2857e9e240417894ad01934eb9ef42
Serhiy Prots <prots.sergiy7@gmail.com>
2019-10-17 21:59:19 +0300

    Change getting the info about current tab, refactor the code

--- app/windows.py
+++ app/windows.py
@@ -3,7 +3,7 @@ This module is used for windows systems
 """
 import win32gui
 import uiautomation as auto
-from app.osAbstract import Os
+from app.os_abstract import Os
 
 
 class Windows(Os):
@@ -11,21 +11,13 @@ class Windows(Os):
         """
         :return: the current working window
         """
-        return win32gui.GetForegroundWindow()
+        return win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
-    def get_active_window_title(self):
-        """
-        :return: the title of current active window
-        """
-        current_window = self.get_active_window()
-        return win32gui.GetWindowText(current_window)
-
-    def get_browser_url(self):
+    def get_browsers_tab(self):
         """
         :return: the representation of current url
         """
         current_window = self.get_active_window()
-        browser_control = auto.ControlFromHandle(current_window)
-        edit = browser_control.EditControl()
-        return 'Web browser: ' + "/" + edit.GetValuePattern().Value
+        web_page_info = current_window.split('-')[0]
+        return f'Web browser: {web_page_info}'
 
